cf50b61efba854dd1717a4d20a5c8b2c9e072321
Chris Lennon <chrislennon@users.noreply.github.com>
2018-07-07 21:39:29 +0200

    standardise intentation

--- bot/index.js
+++ bot/index.js
@@ -3,31 +3,35 @@ const Discord = require('discord.js');
 // Auth token
 const token = process.env.AUTH_TOKEN;
 if (!token) {
-  console.log('No auth token found, please set the AUTH_TOKEN environment variable.\n');
-  process.exit();
+    console.log('No auth token found, please set the AUTH_TOKEN environment variable.\n');
+    process.exit();
 }
 
 // Init bot
 const bot = new Discord.Client();
 bot.on('ready', () => {
-  console.log('Bot connected');
+    console.log('Bot connected');
 });
 
 // Handle messages
 bot.on('message', message => {
-  if (message.author.bot) {
-    return;
-  }
 
-  if (message.system) {
-    return;
-  }
+    // Do not respond to messages from bots
+    if (message.author.bot) {
+        return;
+    }
 
-  try {
-    message.reply("It Works!");
-  } catch (e) {
-    console.error(e.stack);
-  }
+    // Do not response to system messages
+    if (message.system) {
+        return;
+    }
+
+    // Reply to user with generic message
+    try {
+        message.reply("It Works!");
+    } catch (e) {
+        console.error(e.stack);
+    }
 });
 
 console.log('Bot started');
